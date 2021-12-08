6c7a2da82f063a0114ec678d61fb833d1b9f604d
Yves Debeer <yves.debeer@gmail.com>
2021-12-08 15:18:31 +0100

    Update - Added page info for Instana

--- src/router/index.js
+++ src/router/index.js
@@ -20,4 +20,17 @@ const router = new VueRouter({
   routes
 })
 
+router.afterEach(to => {
+  let pageName = to.path
+  if (to.matched && to.matched.length > 0 && to.matched[0].path) {
+    pageName = to.matched[0].path
+  }
+
+  // eslint-disable-next-line no-console
+  console.log('Set page to', pageName)
+
+  // eslint-disable-next-line no-undef
+  ineum('page', pageName)
+})
+
 export default router
